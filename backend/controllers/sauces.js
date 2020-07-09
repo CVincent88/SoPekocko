bca563f689c1fe71b20d80566a41a0cba9b58a6d
CVincent88 <come.vincent@live.fr>
2020-07-09 13:01:30 -0400

    Modification du modèle Sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,7 +1,7 @@
 const Sauce = require('../models/Sauce');
 
 exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce); 
+    const sauceObject = JSON.parse(req.body.sauce);
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -31,7 +31,7 @@ exports.deleteSauce = (req, res, next) => {
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id})
         .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({error}));
+        .catch(error => res.status(404).json({error: error}));
 };
 
 exports.getAllSauces = (req, res, next) => {
@@ -39,3 +39,4 @@ exports.getAllSauces = (req, res, next) => {
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({error}));
 };
+
